9c1c2bc6026cba16328bb4d0d91539122fc33fe4
Alfian Hidayah <alfian.hidayah12@gmail.com>
2020-07-14 22:45:04 +0700

    menambahkan authentikasi API

--- app/src/main/java/com/example/biod/api/RetrofitClient.java
+++ app/src/main/java/com/example/biod/api/RetrofitClient.java
@@ -1,5 +1,11 @@
 package com.example.biod.api;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,10 +14,27 @@ public class RetrofitClient {
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .addInterceptor(
+                    new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request original = chain.request();
+
+                            Request.Builder requestBuilder = original.newBuilder()
+                                    .addHeader("BIOD-API-KEY", "biodapi130413");
+
+                            Request request = requestBuilder.build();
+                            return chain.proceed(request);
+                        }
+                    }
+            ).build();
+
     private RetrofitClient(){
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 
